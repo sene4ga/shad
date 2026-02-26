9614939685eab29ff380c67659c02d201cde72b8
Arsen4ik <Arsen4ik@MacBook-Pro-Arsenij.local>
2026-02-26 16:27:13 +0300

    save

--- 01.1.Regex/tasks/str_literal/str_literal.py
+++ 01.1.Regex/tasks/str_literal/str_literal.py
@@ -1,9 +1,26 @@
 import re
 
+RE_STR_LITERAL = r'''
+^\s*
+(
+    "
+    (?:
+        \\[nt\\"]        # \n \t \\ \"
+        | [^\\\n\t"]
+    )*
+    "
+  |
+    '
+    (?:
+        \\[nt\\']        # \n \t \\ \'
+        | [^\\\n\t']
+    )*
+    '
+)
+'''
 
-RE_STR_LITERAL = ...
+RE_STR_LITERAL = re.compile(RE_STR_LITERAL, re.VERBOSE)
 
 
 def f_repr_str_literal(m: re.Match[str]) -> str:
-    ...
-
+    return m.group(1)

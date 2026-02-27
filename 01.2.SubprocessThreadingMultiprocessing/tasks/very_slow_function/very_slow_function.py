8ba434ee9242ea35d648b2a2da4fbf40d0bc180a
Arsen4ik <Arsen4ik@MacBook-Pro-Arsenij.local>
2026-02-28 02:36:15 +0300

    save

--- 01.2.SubprocessThreadingMultiprocessing/tasks/very_slow_function/very_slow_function.py
+++ 01.2.SubprocessThreadingMultiprocessing/tasks/very_slow_function/very_slow_function.py
@@ -1,4 +1,5 @@
 import time
+from os import cpu_count
 
 
 def very_slow_function(x: int) -> int:
@@ -15,6 +16,8 @@ def calc_squares_simple(bound: int) -> list[int]:
     :param bound: positive upper bound for range
     :return: list of squared numbers
     """
+    ans = [very_slow_function(i) for i in range(bound)]
+    return ans
 
 
 def calc_squares_multithreading(bound: int) -> list[int]:
@@ -23,6 +26,25 @@ def calc_squares_multithreading(bound: int) -> list[int]:
     :param bound: positive upper bound for range
     :return: list of squared numbers
     """
+    from threading import Thread
+    vector = list(range(bound))
+    res = []
+    threads = []
+
+    def worker(i):
+        ans = very_slow_function(i)
+        res.append(ans)
+
+    for arg in vector:
+        x = Thread(target=worker, args=(arg,))
+        x.start()
+        threads.append(x)
+
+    for thread in threads:
+        thread.join()
+
+    return res
+
 
 
 def calc_squares_multiprocessing(bound: int) -> list[int]:
@@ -31,3 +53,9 @@ def calc_squares_multiprocessing(bound: int) -> list[int]:
     :param bound: positive upper bound for range
     :return: list of squared numbers
     """
+    from multiprocessing import Pool
+    vector = list(range(bound))
+    with Pool(processes=cpu_count()) as pool:
+        x = pool.map(very_slow_function, vector)
+        return x
+

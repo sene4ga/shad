de7d2a22ee6346cf80e663dd3ebff2dc0763b958
Arsen4ik <Arsen4ik@MacBook-Pro-Arsenij.local>
2026-02-28 00:27:26 +0300

    add

--- 01.2.SubprocessThreadingMultiprocessing/tasks/sort_tsv/sort_tsv.py
+++ 01.2.SubprocessThreadingMultiprocessing/tasks/sort_tsv/sort_tsv.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import subprocess
 
 
 def python_sort(file_in: Path, file_out: Path) -> None:
@@ -7,6 +8,19 @@ def python_sort(file_in: Path, file_out: Path) -> None:
     :param file_in: tsv file to read from
     :param file_out: tsv file to write to
     """
+    file_in = Path(file_in)
+    file_out = Path(file_out)
+    with file_in.open("r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    def sortl(line):
+        col1, col2 = line.strip("\n").split("\t")
+        return int(col2), col1
+
+    sort = sorted(lines, key=sortl)
+
+    with file_out.open("w", encoding="utf-8") as f:
+        f.writelines(sort)
 
 
 def util_sort(file_in: Path, file_out: Path) -> None:
@@ -15,3 +29,7 @@ def util_sort(file_in: Path, file_out: Path) -> None:
     :param file_in: tsv file to read from
     :param file_out: tsv file to write to
     """
+    subprocess.run(
+        ["sort", "-t", "\t", "-k", "2n", "-k", "1,1", "-o",  file_out, file_in],
+        check=True,
+    )

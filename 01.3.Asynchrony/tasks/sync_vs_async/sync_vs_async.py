6155a17a1e20b27a292ba8d4001880209d91fd85
Arsen4ik <Arsen4ik@MacBook-Pro-Arsenij.local>
2026-02-28 22:32:44 +0300

    save

--- 01.3.Asynchrony/tasks/sync_vs_async/sync_vs_async.py
+++ 01.3.Asynchrony/tasks/sync_vs_async/sync_vs_async.py
@@ -1,4 +1,6 @@
+import threading
 import aiohttp
+import asyncio
 import requests
 
 
@@ -9,6 +11,8 @@ async def async_fetch(session: aiohttp.ClientSession, url: str) -> str:
     :param url: target http url
     :return: fetched text
     """
+    async with session.get(url) as response:
+        return await response.text()
 
 
 async def async_requests(urls: list[str]) -> list[str]:
@@ -17,6 +21,10 @@ async def async_requests(urls: list[str]) -> list[str]:
     :param urls: list of http urls ot fetch
     :return: list of fetched texts
     """
+    async with aiohttp.ClientSession() as session:
+        tasks = [asyncio.create_task(async_fetch(session, url)) for url in urls]
+        results = await asyncio.gather(*tasks)
+        return list(results)
 
 
 def sync_fetch(session: requests.Session, url: str) -> str:
@@ -26,6 +34,7 @@ def sync_fetch(session: requests.Session, url: str) -> str:
     :param url: target http url
     :return: fetched text
     """
+    return session.get(url).text
 
 
 def threaded_requests(urls: list[str]) -> list[str]:
@@ -34,3 +43,20 @@ def threaded_requests(urls: list[str]) -> list[str]:
     :param urls: list of http urls ot fetch
     :return: list of fetched texts
     """
+    session = requests.Session()
+    results = []
+    threads = []
+
+    def worker(url):
+        results.append(sync_fetch(session, url))
+
+    for url in urls:
+        thread = threading.Thread(target=worker, args=(url,))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    return results
+

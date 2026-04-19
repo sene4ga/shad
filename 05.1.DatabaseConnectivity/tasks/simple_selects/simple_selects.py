a1a706c1cecef0a78bbe348c4a65e4d9c1a510f8
Arsen4ik <nikitin.arsenii@phystech.edu>
2026-04-19 12:06:05 +0300

    save

--- 05.1.DatabaseConnectivity/tasks/simple_selects/simple_selects.py
+++ 05.1.DatabaseConnectivity/tasks/simple_selects/simple_selects.py
@@ -8,7 +8,14 @@ class DataBaseHandler:
         Initialize all the context for working with database here
         :param sqlite_database_name: path to the sqlite3 database file
         """
-        pass
+        self.db_path = sqlite_database_name
+        # Явно указываем, что переменная может быть либо None, либо Connection
+        self.conn: tp.Optional[sqlite3.Connection] = None
+
+        self.init()
+
+    def init(self):
+        self.conn = sqlite3.connect(self.db_path)
 
     def get_most_expensive_track_names(self, number_of_tracks: int) -> tp.Sequence[tuple[str]]:
         """
@@ -18,7 +25,20 @@ class DataBaseHandler:
         keywords: SELECT, ORDER BY, LIMIT
         :return:
         """
-        pass
+        # Убеждаем линтер (и себя), что коннект точно существует
+        assert self.conn is not None
+
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            SELECT Name
+            FROM tracks
+            ORDER BY UnitPrice DESC, TrackId ASC
+            LIMIT ?
+            """,
+            (number_of_tracks,)
+        )
+        return cursor.fetchall()
 
     def get_tracks_of_given_genres(self, genres: tp.Sequence[str], number_of_tracks: int) -> tp.Sequence[tuple[str]]:
         """
@@ -29,7 +49,21 @@ class DataBaseHandler:
         keywords: JOIN, WHERE, IN
         :return:
         """
-        pass
+        assert self.conn is not None
+
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            SELECT t.Name
+            FROM tracks t
+            JOIN genres g ON t.GenreId = g.GenreId
+            WHERE g.Name IN ({})
+            ORDER BY t.Milliseconds ASC
+            LIMIT ?
+            """.format(",".join(["?"] * len(genres))),
+            (*genres, number_of_tracks)
+        )
+        return cursor.fetchall()
 
     def get_tracks_that_belong_to_playlist_found_by_name(self, name_needle: str) -> tp.Sequence[tuple[str, str]]:
         """
@@ -41,7 +75,20 @@ class DataBaseHandler:
         keywords: JOIN, WHERE, LIKE
         :return:
         """
-        pass
+        assert self.conn is not None
+
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            SELECT t.Name, p.Name
+            FROM tracks t
+            JOIN playlist_track pt ON t.TrackId = pt.TrackId
+            JOIN playlists p ON pt.PlaylistId = p.PlaylistId
+            WHERE p.Name LIKE ?
+            """,
+            (f"%{name_needle}%",)
+        )
+        return cursor.fetchall()
 
     def teardown(self) -> None:
         """
@@ -49,4 +96,6 @@ class DataBaseHandler:
         Do anything that may be needed or leave blank
         :return:
         """
-        pass
+        if self.conn:
+            self.conn.close()
+            self.conn = None

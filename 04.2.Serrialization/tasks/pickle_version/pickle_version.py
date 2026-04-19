23b90f64f6d4504642a822c5008d8ebb59767173
Arsen4ik <nikitin.arsenii@phystech.edu>
2026-04-19 10:46:56 +0300

    pickle

--- 04.2.Serrialization/tasks/pickle_version/pickle_version.py
+++ 04.2.Serrialization/tasks/pickle_version/pickle_version.py
@@ -3,8 +3,8 @@ import dataclasses
 
 @dataclasses.dataclass
 class PickleVersion:
-	is_new_format: bool
-	version: int
+    is_new_format: bool
+    version: int
 
 
 def get_pickle_version(data: bytes) -> PickleVersion:
@@ -14,3 +14,7 @@ def get_pickle_version(data: bytes) -> PickleVersion:
     :param data: serialized object in pickle format.
     :return: protocol version.
     """
+    if len(data) >= 2 and data[0] == 0x80:
+        return PickleVersion(True, data[1])
+
+    return PickleVersion(False, -1)

3a627f5afd3ecf520a52d200d087b812bbf51741
Arsen4ik <nikitin.arsenii@phystech.edu>
2026-04-30 02:06:01 +0300

    6.1

--- 06.1.RestApi/tasks/url_shortener/url_shortener.py
+++ 06.1.RestApi/tasks/url_shortener/url_shortener.py
@@ -6,3 +6,44 @@ from pydantic import BaseModel
 
 
 app = FastAPI()
+
+class ToShort(BaseModel):
+    url: str
+
+class Shorted(BaseModel):
+    url: str
+    key : str
+
+keysdb = {}
+urlsdb = {}
+
+@app.post('/shorten', response_model=Shorted, status_code=201)
+def short_url(data : ToShort):
+    if data.url in urlsdb:
+        return Shorted(key=urlsdb[data.url], url=data.url)
+
+    key = str(uuid.uuid4())
+    keysdb[key] = data.url
+    urlsdb[data.url] = key
+
+    return Shorted(key=key, url=data.url)
+
+
+@app.get('/go/{key}'
+        , status_code=307
+        , responses=
+         {
+             307: {
+                "content": {
+                    "application/json": {
+                        "schema": {"title": "Response Redirect To Url Go  Key  Get"}
+                    }
+                }
+             }
+        }
+         )
+def redirect_to_url(key : str):
+    url = keysdb.get(key)
+    if not url:
+        raise HTTPException(status_code=404, detail='Key not found')
+    return RedirectResponse(url=url, status_code=307)
